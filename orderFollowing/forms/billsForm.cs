8bd27cbd36fdab8e3847cf5bf80ee343d29df08a
electricianSmurf <yucel.rifatt@gmail.com>
2021-04-08 16:19:01 +0300

    designed bills form

--- orderFollowing/forms/billsForm.cs
+++ orderFollowing/forms/billsForm.cs
@@ -16,5 +16,28 @@ namespace orderFollowing.forms
         {
             InitializeComponent();
         }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            mainForm mainForm = new mainForm();
+            mainForm.ShowDialog();
+            this.Close();
+        }
+
+        private void btnOpenBills_Click(object sender, EventArgs e)
+        {
+            lblTitle.Text = "Open Bills";
+        }
+
+        private void btnClosedBills_Click(object sender, EventArgs e)
+        {
+            lblTitle.Text = "Closed Bills";
+        }
+
+        private void dGridView_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
     }
 }
