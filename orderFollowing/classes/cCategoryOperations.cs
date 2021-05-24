361d978e01862385e2dccad5e7569560b42e79bc
electricianSmurf <yucel.rifatt@gmail.com>
2021-05-24 17:43:42 +0300

    created&coded cCategoryOperations class

---
+++ orderFollowing/classes/cCategoryOperations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace orderFollowing//.classes
+{
+    class cCategoryOperations
+    {
+        cSqlGeneralCommands generalSqlCommands = new cSqlGeneralCommands();
+        public string sqlQuery;
+        private SqlDataAdapter dataAdapter;
+        public DataTable dataTable;
+
+        public void getCategoryNames()
+        {
+            try
+            {
+                generalSqlCommands.connectionString.Open();
+                generalSqlCommands.sqlCommand = new SqlCommand(sqlQuery, generalSqlCommands.connectionString);
+                dataTable = new DataTable();
+                dataAdapter = new SqlDataAdapter(generalSqlCommands.sqlCommand);
+                dataAdapter.Fill(dataTable);
+                generalSqlCommands.connectionString.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Connection Failed!");
+            }
+        }
+    }
+}
